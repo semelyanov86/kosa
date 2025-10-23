fa24f92cae0b4be9977364d60a61b5b690140830
Sergey Emelyanov <me@sergeyem.eu>
2025-10-23 16:16:32 +0200

    fix api prefixes

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/token', [\App\Http\Controllers\TokenController::class, 'create'])->name('token.create');
-Route::get('/contacts', [\App\Http\Controllers\Api\ContactsController::class, 'index'])->name('contacts.index');
-Route::get('/events', [\App\Http\Controllers\Api\EventApiController::class, 'index'])->name('events.index');
+Route::post('/token', [\App\Http\Controllers\TokenController::class, 'create'])->name('api.token.create');
+Route::get('/contacts', [\App\Http\Controllers\Api\ContactsController::class, 'index'])->name('api.contacts.index');
+Route::get('/events', [\App\Http\Controllers\Api\EventApiController::class, 'index'])->name('api.events.index');
